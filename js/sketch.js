7b1efa61a8ad0d842d5b308c8f6ba16e2a592670
bmetenko <bohdanmetenko@outlook.com>
2022-08-16 20:27:06 -0400

    Random p5.js grass png render

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,26 @@
+let img;
+let x_max = 600;
+let y_max = 400;
+let pos;
+
+function preload() {
+  img = loadImage('grass.png');
+  pos = {"x": random(x_max), "y": random(y_max)};
+}
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(x_max, y_max);
+    // line(15, 25, 70, 90);
+    // drawingContext.shadowOffsetX = 5;
+    // drawingContext.shadowOffsetY = -5;
+    // drawingContext.shadowBlur = 10;
+    // drawingContext.shadowColor = "black";
+    // background(200);
+    // ellipse(width/2, height/2, 50, 50);
+
+    image(img, pos["x"], pos["y"]);
 }
 
 function draw() {
-    background(220);
-    ellipse(50,50,80,80);
+
 }
