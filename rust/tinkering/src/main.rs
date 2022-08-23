d27b58a9753e04330a147cd5f7e6a2ec0b49d203
bmetenko <bohdanmetenko@outlook.com>
2022-08-22 22:48:24 -0400

    rust - struct functions

--- rust/tinkering/src/main.rs
+++ rust/tinkering/src/main.rs
@@ -1,3 +1,28 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+
+impl Rectangle {
+    fn area(&self) -> i32 {
+        self.height * self.width
+    }
+
+    fn diagonal(&self) -> f32 {
+        let mut out: f32 = (self.height.pow(2) + self.width.pow(2)) as f32;
+        out = out.sqrt();
+        out
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = Rectangle {
+        width: 4,
+        height: 3
+    };
+
+    println!("rect area: {}", a.area());
+    println!("rect diagonal: {}", a.diagonal());
+
 }
