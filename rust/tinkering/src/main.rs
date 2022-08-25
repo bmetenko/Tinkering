c7773a501b9ddcbfb834d491b8dd37754719422b
bmetenko <bohdanmetenko@outlook.com>
2022-08-24 20:48:16 -0400

    rust - M1 working UI finally (druid)

--- rust/tinkering/src/main.rs
+++ rust/tinkering/src/main.rs
@@ -1,28 +1,22 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: i32,
-    height: i32,
+use druid::widget::{Button, Flex, Label};
+use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc};
+
+fn main() -> Result<(), PlatformError> {
+    let main_window = WindowDesc::new(ui_builder);
+    let data = 0_u32;
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(data)
 }
 
-impl Rectangle {
-    fn area(&self) -> i32 {
-        self.height * self.width
-    }
+fn ui_builder() -> impl Widget<u32> {
+    // The label text will be computed dynamically based on the current locale and count
+    let text =
+        LocalizedString::new("hello-counter").with_arg("count", |data: &u32, _env| (*data).into());
+    let label = Label::new(text).padding(5.0).center();
+    let button = Button::new("increment")
+        .on_click(|_ctx, data, _env| *data += 1)
+        .padding(5.0);
 
-    fn diagonal(&self) -> f32 {
-        let mut out: f32 = (self.height.pow(2) + self.width.pow(2)) as f32;
-        out = out.sqrt();
-        out
-    }
-}
-
-fn main() {
-    let a = Rectangle {
-        width: 4,
-        height: 3
-    };
-
-    println!("rect area: {}", a.area());
-    println!("rect diagonal: {}", a.diagonal());
-
+    Flex::column().with_child(label).with_child(button)
 }
