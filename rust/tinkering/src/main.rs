5d61f37adf4afba70451fe8a74c707452051ad77
bmetenko <bohdanmetenko@outlook.com>
2022-08-24 21:00:44 -0400

    rust - attempting second counter logic

--- rust/tinkering/src/main.rs
+++ rust/tinkering/src/main.rs
@@ -4,6 +4,7 @@ use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, Wind
 fn main() -> Result<(), PlatformError> {
     let main_window = WindowDesc::new(ui_builder);
     let data = 0_u32;
+    let data0 = 1_u32;
     AppLauncher::with_window(main_window)
         .use_simple_logger()
         .launch(data)
@@ -13,10 +14,18 @@ fn ui_builder() -> impl Widget<u32> {
     // The label text will be computed dynamically based on the current locale and count
     let text =
         LocalizedString::new("hello-counter").with_arg("count", |data: &u32, _env| (*data).into());
-    let label = Label::new(text).padding(5.0).center();
+    let label = Label::new(text).padding(10.0).center();
+
+    let text0 = LocalizedString::new("hello-counter").with_arg("count", |data0: &u32, _env| (*data0).into());
+    let label0 = Label::new(text0).padding(10.0).center();
+
     let button = Button::new("increment")
         .on_click(|_ctx, data, _env| *data += 1)
         .padding(5.0);
 
-    Flex::column().with_child(label).with_child(button)
+    let button0 = Button::new("second increment")
+        .on_click(|_ctx, data0, _env| *data0 +=2)
+        .padding(10.0);
+
+    Flex::column().with_child(label).with_child(button).with_child(label0).with_child(button0)
 }
