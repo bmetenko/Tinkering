cfa20b874b573af44a185153d40c763ea5b174b9
bmetenko <bohdanmetenko@outlook.com>
2022-08-28 22:52:59 -0400

    go - tui library tinkering

--- go/go.mod
+++ go/go.mod
@@ -3,7 +3,16 @@ module example/gota-test
 go 1.19
 
 require (
+	github.com/gdamore/encoding v0.0.0-20151215212835-b23993cbb635 // indirect
+	github.com/gdamore/tcell v1.1.0 // indirect
 	github.com/go-gota/gota v0.12.0 // indirect
+	github.com/jroimartin/gocui v0.5.0 // indirect
+	github.com/lucasb-eyer/go-colorful v0.0.0-20180709185858-c7842319cf3a // indirect
+	github.com/marcusolsson/tui-go v0.4.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	gonum.org/v1/gonum v0.9.1 // indirect
 )
---
+++ go/tui.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"log"
+
+	"github.com/marcusolsson/tui-go"
+)
+
+type StyledBox struct {
+	Style string
+	*tui.Box
+}
+
+func (s *StyledBox) Draw(p *tui.Painter) {
+	p.WithStyle(s.Style, func(p *tui.Painter) {
+		s.Box.Draw(p)
+	})
+}
+
+func main() {
+
+	t := tui.NewTheme()
+	normal := tui.Style{Bg: tui.ColorWhite, Fg: tui.ColorBlack}
+
+	t.SetStyle("normal", normal)
+	t.SetStyle("list.item", tui.Style{Bg: tui.ColorYellow, Fg: tui.ColorCyan})
+	t.SetStyle("list.item.selected", tui.Style{Bg: tui.ColorRed, Fg: tui.ColorYellow})
+	t.SetStyle("label.warning", tui.Style{Bg: tui.ColorWhite, Fg: tui.ColorBlack})
+	t.SetStyle("label.fatal", tui.Style{Bg: tui.ColorDefault, Fg: tui.ColorRed})
+	t.SetStyle("label.emphasis", tui.Style{Bold: tui.DecorationOn, Underline: tui.DecorationOn, Bg: tui.ColorRed})
+	t.SetStyle("bsod", tui.Style{Bg: tui.ColorCyan, Fg: tui.ColorWhite})
+
+	okay := tui.NewLabel("Everything is fine.")
+
+	l := tui.NewList()
+	l.SetFocused(true)
+	l.AddItems("First row", "Second row", "Third row", "Fourth row", "Fifth row", "Sixth row")
+	l.SetSelected(0)
+
+	warning := tui.NewLabel("WARNING: This is a warning")
+	warning.SetStyleName("warning")
+
+	fatal := tui.NewLabel("FATAL: Cats and dogs are now living together.")
+	fatal.SetStyleName("fatal")
+
+	message1 := tui.NewLabel("This is an ")
+	emphasis := tui.NewLabel("important")
+	message2 := tui.NewLabel(" message from our sponsors.")
+	message := &StyledBox{
+		Style: "bsod",
+		Box:   tui.NewHBox(message1, emphasis, message2, tui.NewSpacer()),
+	}
+
+	emphasis.SetStyleName("emphasis")
+
+	okay2 := tui.NewLabel("Everything is still fine.")
+
+	root := tui.NewVBox(okay, l, warning, fatal, message, okay2)
+
+	ui, err := tui.New(root)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ui.SetTheme(t)
+	ui.SetKeybinding("Esc", func() { ui.Quit() })
+	ui.SetKeybinding("q", func() { ui.Quit() })
+	ui.SetKeybinding("Ctrl+C", func() { ui.Quit() })
+
+	ui.SetKeybinding("p", func() {
+		// println(l.Selected())
+		l.SetSelected((l.Selected() + 1) % l.Length())
+		return
+	})
+
+	ui.SetKeybinding("o", func() {
+		// println(l.Selected())
+		if l.Selected()-1 != -1 {
+
+		}
+
+		l.SetSelected((l.Selected() - 1) % l.Length())
+
+		return
+	})
+
+	if err := ui.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
