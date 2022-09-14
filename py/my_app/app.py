d3d775b9b81a196de0fb293a42ae211551b2969f
bmetenko <bohdanmetenko@outlook.com>
2022-09-14 00:07:33 -0400

    py - shiny reactivity attempts

--- py/my_app/app.py
+++ py/my_app/app.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from shiny import App, render, ui, reactive
+import rich
 
 time_choices: dict[str, str] = {"t0": "5 seconds", "t1": "30 seconds", "t2": "1 minute"}
 seconds_map: dict[str, int] = {"t0": 5, "t1": 30, "t2": 60}
@@ -17,26 +18,34 @@ app_ui = ui.page_fluid(
 
 
 def server(input, output, session):
+
+    x = reactive.Value("")
+
     @output
     @render.text
     def txt():
         return f"n*2 is {input.n() * 2}"
 
-    @output
-    @render.text
+    @reactive.Effect
     @reactive.event(input.start_timer)
-    async def timer():
+    async def timer_run():
         time_to_wait = 0
         print(input.time_choice())
         for val in input.time_choice():
             time_to_wait += seconds_map[val]
         
         while time_to_wait > 0:
-            print(f"waiting: {time_to_wait} seconds")
+            x.set(f"waiting: {time_to_wait} seconds")
             time_to_wait -= 1
             await asyncio.sleep(1)
+
+        x.set("Timer Done.")
+
         
-        return "Timer done."
+    @output
+    @render.text
+    def timer():
+        return str(x())
 
 
 
