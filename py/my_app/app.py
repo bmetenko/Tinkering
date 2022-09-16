8f9b3651c246eeeb825b00daace80523a0d1abb2
bmetenko <bohdanmetenko@outlook.com>
2022-09-16 08:13:11 -0400

    py - shiny notification modal timer workaround

--- py/my_app/app.py
+++ py/my_app/app.py
@@ -10,7 +10,7 @@ seconds_map: dict[str, int] = {"t0": 5, "t1": 30, "t2": 60}
 app_ui = ui.page_fluid(
     ui.h2("Hello Shiny!"),
     ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
+    ui.input_text("txt", ""),
     ui.input_checkbox_group("time_choice", "Supply time to wait:", time_choices),
     ui.input_action_button("start_timer", "Start timer!"), 
     ui.output_text_verbatim("timer")
@@ -20,41 +20,24 @@ app_ui = ui.page_fluid(
 
 def server(input, output, session):
 
-    x = reactive.Value("No timer started.")
-
-    @output
-    @render.text
-    @reactive.poll(x)
-    def txt():
-        print(x())
-        # return f"x*2 is {x() * 2}"
+    x = reactive.Value(0)
+    timer_done = reactive.Value(True)
+    n: int = 0
 
     @reactive.Effect
     @reactive.event(input.start_timer)
     def timer_run():
+        # reactive.invalidate_later(0.5)
         time_to_wait = 0
-        print(input.time_choice())
         for val in input.time_choice():
             time_to_wait += seconds_map[val]
-            print(time_to_wait)
-        
-        while time_to_wait > 0:
-            x.set(f"waiting: {time_to_wait} seconds")
-            print(x())
-            time_to_wait -= 1
-            time.sleep(1)
 
-        else:
-            print("timer done...")
-            x.set("Timer Done.")
-
-        
-    @output
-    @render.text
-    @reactive.Calc
-    def timer():
-        return str(x())
-
+        x.set(time_to_wait)
+        id = ui.notification_show(
+            f"{x()} second timer, running...", 
+            duration=time_to_wait,
+            close_button=True
+            )
 
 
 app = App(app_ui, server)
