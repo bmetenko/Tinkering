2a7c19eb8d3427db55edf600d3c52114e1fd7fd3
bmetenko <bohdanmetenko@outlook.com>
2022-09-30 23:21:16 -0400

    py - track mouse position after picture found x, y

--- py/picture_find.py
+++ py/picture_find.py
@@ -6,4 +6,19 @@ found = pyautogui.locateOnScreen("1.png", grayscale=True, confidence=.5)
 print(found)
 
 x, y = pyautogui.locateCenterOnScreen("1.png", grayscale=True, confidence=.70)
+print(x, y)
 pyautogui.click(x, y)
+
+
+# Tracking mouse position
+import sys
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+        
+except KeyboardInterrupt:
+    print('\n')
