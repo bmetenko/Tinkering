e90d15e7c8720fc44eadc14173383965b54e86e4
bmetenko <bohdanmetenko@outlook.com>
2022-10-07 22:48:54 -0400

    py - color thief main value

--- py/picture_binning.py
+++ py/picture_binning.py
@@ -1,6 +1,8 @@
-from ast import arg
 from PIL import Image
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import colorthief
 
 parser = argparse.ArgumentParser(description="Image histogram creator.")
 
@@ -10,8 +12,9 @@ def main():
     args = parser.parse_args()
     print(args)
 
-    img = Image.open('%s'%(args.image_path))
-    print(img)
+    img = colorthief.ColorThief('%s'%(args.image_path))
+
+    plt.imshow(([[img.get_color(quality=1)]]))
     
 
 if __name__ == "__main__":
