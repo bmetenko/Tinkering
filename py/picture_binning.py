bca3a46ab0097423b04da031f6748d0e6e9cb92b
bmetenko <bohdanmetenko@outlook.com>
2022-10-08 23:53:10 -0400

    py - rgb distance

--- py/picture_binning.py
+++ py/picture_binning.py
@@ -4,6 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import colorthief
 
+def rgb_distance(x: tuple[int], y: tuple[int]):
+    r1, g1, b1 = x
+    r2, g2, b2 = y
+    d = np.sqrt((r2-r1)**2+(g2-g1)**2+(b2-b1)**2)
+    return d
+
+
 parser = argparse.ArgumentParser(description="Image histogram creator.")
 
 parser.add_argument("-i", "--image_path", dest="image_path", help="Image path", required=True)
@@ -15,7 +22,10 @@ def main():
     img = colorthief.ColorThief('%s'%(args.image_path))
 
     plt.imshow(([[img.get_color(quality=1)]]))
-    
+
+    plt.imshow([[i for i in img.get_palette(5)]])
+
+
 
 if __name__ == "__main__":
     main()
