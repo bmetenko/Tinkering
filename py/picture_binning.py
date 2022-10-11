e93db3a0c7d4832d89b7a13783462fa3ed533653
bmetenko <bohdanmetenko@outlook.com>
2022-10-10 22:46:14 -0400

    py - palette distance merge calculation options

--- py/picture_binning.py
+++ py/picture_binning.py
@@ -1,20 +1,33 @@
+from enum import Enum, auto
+
 from PIL import Image
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import colorthief
 
+class PalCalc(Enum):
+    Sum = auto()
+    Mean = auto()
+    Multiply = auto()
+    
+
 def rgb_distance(x: tuple[int], y: tuple[int]):
     r1, g1, b1 = x
     r2, g2, b2 = y
-    
+
     d = np.sqrt((
         r2-r1)**2+(g2-g1)**2+(b2-b1)**2
     )
 
     return d
 
-def palette_distance(pal1, pal2, check_iterations=1):
+def palette_distance(
+    pal1, 
+    pal2, 
+    check_iterations: int = 1,
+    calculation: PalCalc = PalCalc.Sum
+    ):
 
     max_iterations = min(len(pal1), len(pal2))
 
@@ -24,10 +37,24 @@ def palette_distance(pal1, pal2, check_iterations=1):
     if max_iterations < check_iterations:
         check_iterations = max_iterations
 
-    out = 0
+    out = 0 if calculation == PalCalc.Sum else 1
 
-    for i in range(check_iterations):
-        out += rgb_distance(pal1[i], pal2[1])
+    
+    if calculation == PalCalc.Sum:
+        for i in range(check_iterations):
+            out += rgb_distance(pal1[i], pal2[i])
+        
+    if calculation == PalCalc.Multiply:
+        for i in range(check_iterations):
+            out *= rgb_distance(pal1[i], pal2[i]) 
+
+    if calculation == PalCalc.Mean:
+        out = np.mean(
+            [
+                rgb_distance(pal1[x], pal2[x]) 
+                for x in range(max_iterations)
+            ]
+        )
 
     return out
 
