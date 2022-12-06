9ce92eb21e2e9d0daec8ed3d7f512d6ab5fb279c
bmetenko <bohdanmetenko@outlook.com>
2022-12-05 22:20:06 -0500

    ruby - simple fringe introduction

---
+++ rb/intro.rb
@@ -0,0 +1,64 @@
+class Introduction
+    def initialize(name = "Astrid")
+        @name = name
+    end
+
+    def say_hello
+        if @name.nil?
+            puts "Hello Noone?"
+        elsif @name.respond_to?("each")
+            @name.each do |iname|
+                puts "Hello #{iname}!"
+            end
+        else
+            puts "Hello #{@name}."
+        end
+    end
+
+    def say_goodbye
+        if @name.nil?
+            puts "..."
+        elsif @name.respond_to?("join")
+            puts "Bye #{@name.join(", ")}."
+        else
+            puts "Bye #{@name}."
+        end
+    end
+
+end
+
+
+introducer = Introduction.new("Astro")
+introducer.say_hello
+introducer.say_goodbye
+
+class Introduction
+    attr_accessor :name
+end
+
+# Class extension code above
+# works on initialized classes
+introducer.name
+
+# __main__ equivalent
+if __FILE__ == $0
+    mg = Introduction.new
+    mg.say_hello
+    mg.say_goodbye
+
+    mg.name = "Observer"
+    mg.say_hello
+    mg.say_goodbye
+
+    mg.name = ["Olivia", "Walter", "Peter",
+    "Broyles", "Etta"]
+    mg.say_hello
+    mg.say_goodbye
+
+    # Change to nil
+    mg.name = nil
+    mg.say_hello
+    mg.say_goodbye
+end
+
+exit
