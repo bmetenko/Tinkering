655be6992fc3fb34fe030f3d5bfbb258a52398bd
bmetenko <bohdanmetenko@outlook.com>
2022-12-09 00:03:50 -0500

    rust - iter subset utf8 add to file

--- rust/utf8-list/src/main.rs
+++ rust/utf8-list/src/main.rs
@@ -1,13 +1,35 @@
+use std::fs::File;
+use std::io::Write;
 
-fn main() {
+fn main() -> std::io::Result<()> {
 
-    let check_utf = vec![240, 159, 141, 137];
-    let s = std::str::from_utf8(&check_utf);
+    let file_path = "utf8.txt";
+    let mut output = File::create(file_path).expect("Unable to create file");
 
-    if s.is_err(){
-        println!("{:?}", check_utf);
-    } else {
-        println!("{:?}", s)
-    }    
+    let x: Vec<u8> = (240..255).collect();
+    let y: Vec<u8> = (150..160).collect();
+    let z: Vec<u8> = (145..155).collect();
+    let a: Vec<u8> = (150..160).collect();
 
+    for x0 in x.iter() {
+        for y0 in y.iter(){
+            for z0 in z.iter(){
+                for a0 in a.iter(){
+                    let check_utf: Vec<u8> = vec![*x0, *y0, *z0, *a0];
+                    println!("{:?}", &check_utf);
+
+                    let s = std::str::from_utf8(&check_utf);
+                
+                    match s {
+                        Err(_s) => println!("{:?}", check_utf),
+                        Ok(s) => writeln!(output, "{:?} : {:?}", check_utf, s).expect("Could not write line...")
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    Ok(())
 }
