9973475d720f4955d3de096020d201994615d78c
bmetenko <bohdanmetenko@outlook.com>
2023-01-03 23:18:52 -0500

    py - fpdf2 footer override both sides

--- py/pdf.py
+++ py/pdf.py
@@ -1,6 +1,13 @@
 import fpdf
 
-pdf = fpdf.FPDF()
+class ExPDF(fpdf.FPDF):
+    def footer(self):
+        self.set_y(-25)
+        self.set_font("helvetica", "I", 16)
+        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="R", new_x="LEFT", new_y=fpdf.YPos.LAST)
+        self.cell(0, 10, "Example PDF", align="L")
+
+pdf = ExPDF()
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
 for x in range(10):
