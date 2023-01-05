6780e97815243705ab6ad54298b41a5301bb365f
bmetenko <bohdanmetenko@outlook.com>
2023-01-04 22:47:33 -0500

    py - fpdf header double title class override

--- py/pdf.py
+++ py/pdf.py
@@ -7,6 +7,15 @@ class ExPDF(fpdf.FPDF):
         self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="R", new_x="LEFT", new_y=fpdf.YPos.LAST)
         self.cell(0, 10, "Example PDF", align="L")
 
+    def header(self):
+        self.set_font("helvetica", "B", 15)
+        self.cell(50)
+        self.cell(60, 10, "Example PDF after 50", border=1, align="C")
+        self.ln(20)
+        self.cell(30)
+        self.cell(60, 10, "Example PDF after 30", border=1, align="C")
+        self.ln(20)
+
 pdf = ExPDF()
 pdf.add_page()
 pdf.set_font("helvetica", "B", 16)
