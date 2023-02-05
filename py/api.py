2cd3799b08c868d82ff8bb707fe6aeb8b6f2b1e9
bmetenko <42018656+bmetenko@users.noreply.github.com>
2023-02-05 03:39:15 +0000

    py - fastapi status return and rect model

--- py/api.py
+++ py/api.py
@@ -1,10 +1,17 @@
+from typing import List
+
 import rectpack as rp
 import fastapi as fa
 from pydantic import BaseModel
 
+class Rect(BaseModel):
+    width: int
+    height: int
+
 
 class Canvas(BaseModel):
     canvas_size: int
+    contents: List[Rect]
 
 
 app = fa.FastAPI()
@@ -15,7 +22,7 @@ def root():
     return {"message": "Attempts at a square packing API based on rect pack."}
 
 
-@app.post("/create/")
+@app.post("/create/", status_code=fa.status.HTTP_201_CREATED)
 def create(canvas: Canvas):
     return canvas
 
