553b113538f9572033a10857c3e001822148e1ef
bmetenko <bohdanmetenko@outlook.com>
2023-03-16 22:59:12 -0400

    py - exploring streamlit extras

--- py/streamlit/optimize_dfs/1_👋 Introduction.py
+++ py/streamlit/optimize_dfs/1_👋 Introduction.py
@@ -1,5 +1,35 @@
 import streamlit as st
+from streamlit_extras.badges import badge
+from streamlit_extras.app_logo import add_logo
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+from streamlit_card import card
+
+c0, c1 = st.columns(2)
+with c0:
+    card(
+        title="Hello!",
+        text="Enjoy your stay!",
+        image="https://cdn.pixabay.com/photo/2015/04/27/11/48/sign-741813_1280.jpg",
+        url="https://www.github.com/bmetenko",
+    )
+
+with c1:
+    card(
+        title="2nd Welcome",
+        text="Not sure why it was needed...",
+        image="https://pandas.pydata.org/static/img/pandas_mark.svg",
+        url='/Theory'
+    )
+
+logo_url = "https://seeklogo.com/images/S/streamlit-logo-1A3B208AE4-seeklogo.com.png"
+add_logo(logo_url)
+
+badge(type="github", name="bmetenko")
 
 st.write(
     """
