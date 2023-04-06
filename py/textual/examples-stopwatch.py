b35a948006a6229e8723972ccbdbcde409dcbecd
bmetenko <bohdanmetenko@outlook.com>
2023-04-06 19:14:45 -0400

    py - textual widget addition / removal hotkeys

--- py/textual/examples-stopwatch.py
+++ py/textual/examples-stopwatch.py
@@ -13,6 +13,8 @@ class Stopwatch(App):
     BINDINGS = [
         ("q", "quit", "Quit app"),
         ("d", "toggle_dark", "Toggle dark mode"),
+        ("a", "add_timer", "Add"),
+        ("r", "remove_timer", "Remove")
         ]
 
     def compose(self) -> ComposeResult:
@@ -22,11 +24,26 @@ class Stopwatch(App):
             StopwatchWidget(),
             StopwatchWidget(),
             StopwatchWidget(),
+            id = "timers"
         )
 
     def action_toggle_dark(self) -> None:
         """"""
         self.dark = not self.dark
+
+    def action_add_timer(self) -> None:
+        """ Adding timer. """
+        new_timer = StopwatchWidget()
+        self.query_one("#timers").mount(new_timer)
+        new_timer.scroll_visible()
+
+    def action_remove_timer(self) -> None:
+        timers = self.query("StopwatchWidget")
+        if timers:
+            timers.last().remove()
+
+    def action_quit(self) -> None:
+        quit()
 
 if __name__ == "__main__":
     app = Stopwatch()
