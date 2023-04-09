1d97fb41468a9d177cdf0594c71e117c6298209f
bmetenko <bohdanmetenko@outlook.com>
2023-04-08 22:47:45 -0400

    py - textual global opacity cycling per addition/destroy of widget

--- py/textual/examples-stopwatch.py
+++ py/textual/examples-stopwatch.py
@@ -1,8 +1,12 @@
 from textual import log
+from textual.color import Color
 from textual.app import App, ComposeResult
 from textual.containers import Container
 from textual.widgets import Header, Footer
 from stopwatch_widget import StopwatchWidget 
+
+global count 
+count = 3
 
 class Stopwatch(App):
     """
@@ -36,13 +40,23 @@ class Stopwatch(App):
         """ Adding timer. """
         new_timer = StopwatchWidget()
         self.query_one("#timers").mount(new_timer)
+        
+        global count
+
+        alpha = 1 - 0.05 * (count % 20)
+        count += 1
+        new_timer.styles.background = Color(19, 224, 24, a=alpha)
+
         new_timer.scroll_visible()
         log(f"Added timer {new_timer}")
 
     def action_remove_timer(self) -> None:
+        global count
+
         timers = self.query("StopwatchWidget")
         
         log("Removed timer: {timers.last()}")
+        count -= 1
 
         if timers:
             timers.last().remove()
