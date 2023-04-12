82fa472a2b329e8f35c1cfd262ef60288efad653
bmetenko <bohdanmetenko@outlook.com>
2023-04-11 20:56:21 -0400

    rust - improve utf8 check code

--- rust/utf8-list/src/main.rs
+++ rust/utf8-list/src/main.rs
@@ -1,35 +1,33 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{Result, Write};
 
-fn main() -> std::io::Result<()> {
-
+fn main() -> Result<()> {
     let file_path = "utf8.txt";
-    let mut output = File::create(file_path).expect("Unable to create file");
+    let mut output = File::create(file_path)?;
 
-    let x: Vec<u8> = (240..255).collect();
-    let y: Vec<u8> = (150..160).collect();
-    let z: Vec<u8> = (145..155).collect();
-    let a: Vec<u8> = (150..160).collect();
+    let x = (200..255).collect::<Vec<u8>>();
+    let y = (150..160).collect::<Vec<u8>>();
+    let z = (145..155).collect::<Vec<u8>>();
+    let a = (150..160).collect::<Vec<u8>>();
 
-    for x0 in x.iter() {
-        for y0 in y.iter(){
-            for z0 in z.iter(){
-                for a0 in a.iter(){
+    for x0 in &x {
+        for y0 in &y {
+            for z0 in &z {
+                for a0 in &a {
                     let check_utf: Vec<u8> = vec![*x0, *y0, *z0, *a0];
                     println!("{:?}", &check_utf);
 
                     let s = std::str::from_utf8(&check_utf);
-                
+
                     match s {
-                        Err(_s) => println!("{:?}", check_utf),
-                        Ok(s) => writeln!(output, "{:?} : {:?}", check_utf, s).expect("Could not write line...")
+                        Err(_) => println!("{:?}", check_utf),
+                        Ok(s) => writeln!(output, "{:?} : {:?}", check_utf, s)?,
                     }
                 }
             }
         }
     }
 
-
-
     Ok(())
 }
+
