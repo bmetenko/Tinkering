bdaf2099ef9c394be0cc7e813a8ad44c863320c7
bmetenko <bohdanmetenko@outlook.com>
2023-08-11 22:01:24 -0400

    py - lets plot? - facet + scatter

--- py/letsplot/begin.py
+++ py/letsplot/begin.py
@@ -2,7 +2,8 @@ import numpy as np
 from lets_plot import (
     LetsPlot, ggplot, geom_density,
     ggsize, scale_fill_brewer, theme,
-    ggsave, aes
+    ggsave, aes, geom_histogram,
+    facet_wrap, geom_bar, geom_point
 )
 
 LetsPlot.setup_html()
@@ -16,16 +17,26 @@ data = dict(
             np.random.normal(0, 1, 250),
             np.random.normal(1, 2, 250)
         )
+    ),
+    skew=np.concatenate(
+        (
+            np.random.normal(-1, 1, 250),
+            np.random.normal(-1, 1, 250)
+        )
     )
 )
 
-main_plot = \
+main_plot = (
     ggplot(
-        data, aes(x='rating', fill='cond')
-    ) + \
-    ggsize(700, 300) + \
-    geom_density(color='dark_green', alpha=.7) + \
-    scale_fill_brewer(type='seq') + \
-    theme(panel_grid_major_x='blank')
+        data, aes(x='rating', y='skew', fill='cond')
+    ) +
+    ggsize(700, 300) +
+    geom_bar() +
+    geom_point() +
+    geom_density(color='dark_green', alpha=.7) +
+    scale_fill_brewer(type='seq') +
+    theme(panel_grid_major_x='blank') +
+    facet_wrap('cond')
+)
 
-ggsave(main_plot, filename="testplot.png")
+ggsave(main_plot, filename="testplot2.png")
