caa22561195df56ea8f5ec7390a92dc5a3519d37
bmetenko <bohdanmetenko@outlook.com>
2023-09-05 22:48:39 -0400

    c - command file arg sha256sum (macOS)

--- c/sha.c
+++ c/sha.c
@@ -3,8 +3,19 @@
 #include <string.h>
 #include <unistd.h>
 
-int main() {
-    FILE* file = popen("shasum -a 256 init_write.txt", "r"); // Open for reading
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    const char *filename = argv[1];
+    char command[1024]; // Assuming a reasonably large buffer for the command
+
+    // Construct the command to calculate the SHA-256 hash
+    snprintf(command, sizeof(command), "shasum -a 256 %s", filename);
+
+    FILE* file = popen(command, "r"); // Open for reading
     if (file == NULL) {
         perror("popen");
         return 1;
@@ -13,7 +24,7 @@ int main() {
     char buffer[65]; // +1 for the null terminator
 
     if (fscanf(file, "%64s", buffer) == 1) {
-        printf("buffer is: %s\n", buffer);
+        printf("Sha256 of %s: \n%s\n", filename, buffer);
     } else {
         fprintf(stderr, "Error reading SHA-256 hash.\n");
     }
