808e40b6d441cf7b149506e9b263afb7fe6fdf97
bmetenko <bohdanmetenko@outlook.com>
2023-09-07 23:16:02 -0400

    c - command multi file arg sha256sum (macOS)

--- c/sha.c
+++ c/sha.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <file1> <file2> ... <fileN>\n", argv[0]);
         return 1;
     }
 
-    const char *filename = argv[1];
-    char command[1024]; // Assuming a reasonably large buffer for the command
+    for (int i = 1; i < argc; i++) {
+        const char *filename = argv[i];
+        char command[1024]; // Assuming a reasonably large buffer for the command
 
-    // Construct the command to calculate the SHA-256 hash
-    snprintf(command, sizeof(command), "shasum -a 256 %s", filename);
+        // Construct the command to calculate the SHA-256 hash
+        snprintf(command, sizeof(command), "shasum -a 256 %s", filename);
 
-    FILE* file = popen(command, "r"); // Open for reading
-    if (file == NULL) {
-        perror("popen");
-        return 1;
+        FILE* file = popen(command, "r"); // Open for reading
+        if (file == NULL) {
+            perror("popen");
+            return 1;
+        }
+
+        char buffer[65]; // +1 for the null terminator
+
+        if (fscanf(file, "%64s", buffer) == 1) {
+            printf("SHA-256 of %s: %s\n", filename, buffer);
+        } else {
+            fprintf(stderr, "Error reading SHA-256 hash for %s.\n", filename);
+        }
+
+        pclose(file);
     }
 
-    char buffer[65]; // +1 for the null terminator
-
-    if (fscanf(file, "%64s", buffer) == 1) {
-        printf("Sha256 of %s: \n%s\n", filename, buffer);
-    } else {
-        fprintf(stderr, "Error reading SHA-256 hash.\n");
-    }
-
-    pclose(file);
     return 0;
 }
