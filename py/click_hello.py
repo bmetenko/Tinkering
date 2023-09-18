3099053273f458fcc78ff5b4758d9a5ccb21e75b
bmetenko <bohdanmetenko@outlook.com>
2023-09-17 22:47:11 -0400

    py - click cli style simple

--- py/click_hello.py
+++ py/click_hello.py
@@ -19,9 +19,20 @@ usage: python -m click_hello --count=2
 )
 def hello(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
+
+    start_of_program()
+
     for x in range(count):
         click.echo(f"Hello {name}!")
 
+    end_of_program()
+
+def start_of_program():
+    click.echo(click.style("- start of program -", blink=True, fg="red", bold=True))
+
+def end_of_program():
+    click.echo(click.style("- end of program -", blink=True, fg="cyan", bold=True))
 
 if __name__ == '__main__':
     hello()
+    
