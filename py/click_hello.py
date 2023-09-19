e9c4fd49277e9e44d37d4af47505b2e1a360b1ce
bmetenko <bohdanmetenko@outlook.com>
2023-09-18 23:55:41 -0400

    py - click cli grouping commands

--- py/click_hello.py
+++ py/click_hello.py
@@ -1,12 +1,14 @@
 import click
 
 """
-usage: python -m click_hello --count=2
->>> prompt name:
+usage: python3 -m click_hello hello --name=Dan --count=2
 """
 
+@click.group()
+def main_group():
+    pass
 
-@click.command()
+@main_group.command()
 @click.option(
     '--count',
     default=1,
@@ -14,25 +16,36 @@ usage: python -m click_hello --count=2
 )
 @click.option(
     '--name',
-    prompt='name',
+    # prompt='name',
     help='Who to greet.'
 )
-def hello(count, name):
+def hello(name, count):
     """Simple program that greets NAME for a total of COUNT times."""
 
-    start_of_program()
+    # start_of_program()
 
     for x in range(count):
         click.echo(f"Hello {name}!")
 
-    end_of_program()
+    # end_of_program()
 
+@main_group.command()
 def start_of_program():
     click.echo(click.style("- start of program -", blink=True, fg="red", bold=True))
 
+@main_group.command()
 def end_of_program():
-    click.echo(click.style("- end of program -", blink=True, fg="cyan", bold=True))
+    click.echo(
+        click.style("- end of", blink=True, fg="cyan", bold=True) + 
+        " " + 
+        click.style("program -", blink=True, fg="yellow", bold=True)
+        )
+
+main_group.add_command(start_of_program)
+main_group.add_command(hello)
+main_group.add_command(end_of_program)
+
 
 if __name__ == '__main__':
-    hello()
+    main_group()
     
