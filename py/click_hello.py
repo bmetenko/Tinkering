467d53d96a80a245a50d691a077e91beae2b9996
bmetenko <bohdanmetenko@outlook.com>
2023-09-19 22:48:46 -0400

    python - click cli chained commands

--- py/click_hello.py
+++ py/click_hello.py
@@ -2,50 +2,75 @@ import click
 
 """
 usage: python3 -m click_hello hello --name=Dan --count=2
+chained call: python3 -m click_hello start hello --name=Dan --count=2 end
+
 """
 
-@click.group()
-def main_group():
-    pass
+
+@click.group(chain=True)
+@click.option('--debug/--no-debug', default=False)
+@click.option(
+    '--count',
+    default=1,
+    help='number of hellos.'
+)
+@click.option(
+    '--name',
+    # prompt='name',
+    help='who to greet.'
+)
+@click.pass_context
+def main_group(ctx, debug, count, name):
+    ctx.ensure_object(dict)
+    ctx.obj['DEBUG'] = debug
+    ctx.obj['name'] = name
+    ctx.obj['count'] = count
 
 @main_group.command()
 @click.option(
     '--count',
     default=1,
-    help='Number of hellos.'
+    help='number of hellos.'
 )
 @click.option(
     '--name',
     # prompt='name',
-    help='Who to greet.'
+    help='who to greet.'
 )
-def hello(name, count):
+@click.pass_context
+def hello(ctx, name, count):
     """Simple program that greets NAME for a total of COUNT times."""
 
     # start_of_program()
+    if not name or not count:
+        name = ctx.obj['name']
+        count = ctx.obj['count']
 
     for x in range(count):
         click.echo(f"Hello {name}!")
 
     # end_of_program()
 
-@main_group.command()
-def start_of_program():
+
+@main_group.command('start')
+@click.pass_context
+def start_of_program(ctx):
+    click.echo(ctx)
     click.echo(click.style("- start of program -", blink=True, fg="red", bold=True))
 
-@main_group.command()
+
+@main_group.command('end')
 def end_of_program():
     click.echo(
-        click.style("- end of", blink=True, fg="cyan", bold=True) + 
-        " " + 
+        click.style("- end of", blink=True, fg="cyan", bold=True) +
+        " " +
         click.style("program -", blink=True, fg="yellow", bold=True)
-        )
+    )
+
 
 main_group.add_command(start_of_program)
 main_group.add_command(hello)
 main_group.add_command(end_of_program)
 
-
 if __name__ == '__main__':
     main_group()
-    
