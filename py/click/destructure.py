e71f43dbab562a3de2e78dfe5806192e860c9ddf
bmetenko <bohdanmetenko@outlook.com>
2023-10-09 21:58:44 -0400

    py - click / rich simple disassembly recursive init

--- py/click/destructure.py
+++ py/click/destructure.py
@@ -19,38 +19,22 @@ first_tree = Tree(
     guide_style="bold black",
 )
 
-for instr in test_dis:
-    print(instr)
-    branch = first_tree.add(
+
+def next_tree(diss, init_tree):
+    for instr in diss:
+        # print(instr)
+        branch = init_tree.add(
             f"[bold yellow]{instr.opname}",
         )
 
-    branch.add(Text(f"Value: {instr.argval}", style='blue'))
+        branch.add(Text(f"Value: {instr.argval}", style='blue'))
 
-    if type(instr.argval).__name__ == 'code':
-        print(dir(instr.argval))
-        next_dis = dis.Bytecode(instr.argval)
+        if type(instr.argval).__name__ == 'code':
+            # print(dir(instr.argval))
+            next_diss = dis.Bytecode(instr.argval)
 
-        for inst in next_dis:
-            branch2 = branch.add(
-                f"[bold yellow]{inst.opname}",
-            )
-            branch2.add(Text(f"Value: {inst.argval}", style='green'))
+            next_tree(next_diss, branch)
 
+
+next_tree(test_dis, first_tree)
 print(first_tree)
-# import types
-#
-# source_py = "click_hello.py"
-#
-# with open(source_py) as f_source:
-#     source_code = f_source.read()
-#
-# byte_code = compile(source_code, source_py, "exec")
-# dis.dis(byte_code)
-#
-# for x in byte_code.co_consts:
-#     if isinstance(x, types.CodeType):
-#         sub_byte_code = x
-#         func_name = sub_byte_code.co_name
-#         print('\nDisassembly of %s:' % func_name)
-#         dis.dis(sub_byte_code)
